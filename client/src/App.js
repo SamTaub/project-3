848a83be0acdcdb1194a1528830c648bf777edfb
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-07 13:36:55 -0400

    Add boilerplate Bootstrap components

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route component={NoMatch} />
