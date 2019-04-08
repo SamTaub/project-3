293a79109d292b39896495832d0a264c5667460f
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-08 09:13:52 -0400

    Refactor row to accommodate passed in styles

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,13 +6,13 @@ import { Container, Row, Col } from "../../components/Grid";
 function Home() {
   return (
     <Container className="bg-transparent">
-      <div className="row justify-content-center align-items-center text-center text-white">
+      <Row styles="justify-content-center align-items-center text-center text-white">
         <div className="col-lg-8">
           <h1 className="mt-5" id="mast">Beadli</h1>
             <p id="subtitle">Make interesting designs with beads.</p>
             <Link className="btn btn-outline-light btn-lg" to="/login" role="button">Get Started</Link>
         </div>
-      </div>
+      </Row>
     </Container>
   );
 }
