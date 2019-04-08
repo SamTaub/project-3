147aa6415ab0bab7f646fb13ebcd937acd809512
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-08 09:08:35 -0400

    Containerize homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
-// import { Container, Row, Col } from "../../components/Grid";
+import { Container, Row, Col } from "../../components/Grid";
 
 function Home() {
   return (
-    <div className="container bg-transparent">
+    <Container className="bg-transparent">
       <div className="row justify-content-center align-items-center text-center text-white">
         <div className="col-lg-8">
           <h1 className="mt-5" id="mast">Beadli</h1>
@@ -13,7 +13,7 @@ function Home() {
             <Link className="btn btn-outline-light btn-lg" to="/login" role="button">Get Started</Link>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
