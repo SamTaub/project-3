df6c3a92b606643e7e76e90fd1b371fe7829fa23
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-09 21:31:12 -0400

    Create basic API util for user authentication

--- client/src/utils/userAPI.js
+++ client/src/utils/userAPI.js
@@ -1,1 +1,31 @@
 import axios from "axios";
+
+export default {
+  // Function that sends a post request to the /api/signup endpoint. Should return a boolean response.
+  signUp: function(username, email, password) {
+    return axios.post("/api/signup", {
+      username: username,
+      email: email,
+      password: password
+    });
+  },
+
+  // Function that sends a post request to the /api/login endpoint. Should return a boolean response.
+  logIn: function(email, password) {
+    return axios.post("api/login", {
+      email: email,
+      password: password
+    });
+  },
+
+  // Get request to logout the current user. Should return a boolean response.
+  logOut: function() {
+    return axios.get("/logout");
+  },
+
+  // Function sends a request to the api/user endpoint to see if req.user exists.
+  // Expects a boolean response.
+  checkAuthStatus: function() {
+    return axios.get("api/user");
+  }
+};
