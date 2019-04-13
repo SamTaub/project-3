de3e04876ced407cc478bc665073956bcad5a931
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-13 14:36:53 -0400

    Add functional colorpicker with background images (looks terrible tho)

--- client/src/components/ColorPicker/ColorPicker.js
+++ client/src/components/ColorPicker/ColorPicker.js
@@ -1,38 +1,42 @@
 import React from "react";
+import colorPalette from "../../utils/colorPalette";
 import "./style.css";
 
 function ColorPicker(props) {
+  let colorArray = Object.entries(colorPalette);
+  let table = [];
+
+  for (let i = 0; i < colorArray.length; i++) {
+    console.log(colorArray[i]);
+    if ((i + 1) % 5 === 0) {
+      table.push(
+        <tr>
+          <td
+            data-value={colorArray[i][0]}
+            style={{ background: `${colorArray[i][0]}`, width: "5px" }}
+          >
+            {colorArray[i][1]}
+          </td>
+        </tr>
+      );
+    } else {
+      table.push(
+        <td
+          data-value={colorArray[i][0]}
+          style={{ background: `${colorArray[i][0]}` }}
+        >
+          {colorArray[i][1]}
+        </td>
+      );
+    }
+  }
+
   return (
     <div className="table-responsive" onClick={props.onChange}>
       <table className="table">
-        <tbody>
-          <tr>
-            <td data-value="#FFF" style={{background: "#FFF" }}>White</td>
-            <td data-value="#00FF00" style={{background: "#00FF00" }}>Green</td>
-            <td data-value="#0000FF" style={{background: "#0000FF" }}>Blue</td>
-            <td data-value="#FFFF00" style={{background: "#FFFF00" }}>Yellow</td>
-            <td data-value="#00FFFF" style={{background: "#00FFFF" }}>Cyan</td>
-          </tr>
-          <tr>
-            <td data-value="#FFF">White</td>
-            <td data-value="#00FF00">Green</td>
-            <td data-value="#0000FF">Blue</td>
-            <td data-value="#FFFF00">Yellow</td>
-            <td data-value="#00FFFF">Cyan</td>
-          </tr>
-        </tbody>
+        <tbody>{table}</tbody>
       </table>
     </div>
-    // <div onChange={props.onChange}>
-    //   <select>
-    //     <option>Select a Color</option>
-    //     <option value="#FFF">White</option>
-    //     <option value="#00FF00">Green</option>
-    //     <option value="#0000FF">Blue</option>
-    //     <option value="#FFFF00">Yellow</option>
-    //     <option value="#00FFFF">Cyan</option>
-    //   </select>
-    // </div>
   );
 }
 
