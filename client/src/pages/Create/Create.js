ce952b6731f70c81c69c9bd7dc7cb147af172541
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-12 20:47:32 -0400

    Move Board, Square and related components into the Board folder

--- client/src/pages/Create/Create.js
+++ client/src/pages/Create/Create.js
@@ -1,65 +1,8 @@
 import React, { Component } from "react";
+import { Board, ColorPicker, ClearButton } from "../../components/Board/Board";
 import "./style.css";
 
-function Square(props) {
-  return (
-    <button
-      className="square"
-      style={{ background: props.value }}
-      onClick={props.onClick}
-    />
-  );
-}
-
-function ColorPicker(props) {
-  return (
-    <div onChange={props.onChange}>
-      <select>
-        <option>Select a Color</option>
-        <option value="#FFF">White</option>
-        <option value="#00FF00">Green</option>
-        <option value="#0000FF">Blue</option>
-        <option value="#FFFF00">Yellow</option>
-        <option value="#00FFFF">Cyan</option>
-      </select>
-    </div>
-  );
-}
-
-function ClearButton(props) {
-  return <button onClick={props.onClick}>Clear Board</button>;
-}
-
-class Board extends Component {
-  renderSquare(value, rowIdx, colIdx) {
-    // console.log(this.state.squares[rowIdx][colIdx]);
-    return (
-      <Square
-        key={colIdx}
-        value={value}
-        onClick={() => this.props.onClick(value, rowIdx, colIdx)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        {this.props.squares.map((row, rowIdx) => {
-          return (
-            <div key={rowIdx} className="board-row" datarow={rowIdx}>
-              {row.map((value, colIdx) =>
-                this.renderSquare(value, rowIdx, colIdx)
-              )}
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
-
-class Create extends React.Component {
+class Create extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -84,10 +27,10 @@ class Create extends React.Component {
   };
 
   handleClick = (value, rowIdx, colIdx) => {
-    let squares = [...this.state.squares];
-    let square = { ...squares[rowIdx][colIdx] };
-    square = this.state.activeColor;
-    squares[rowIdx][colIdx] = square;
+    let squares = [...this.state.squares]; // Create a copy of the square values.
+    let square = { ...squares[rowIdx][colIdx] }; // Find our particular square.
+    square = this.state.activeColor; // Set new value of square equal to active color.
+    squares[rowIdx][colIdx] = square; // Set color at the copied location.
     this.setState({ squares }, () => console.log(this.state.squares));
   };
 
