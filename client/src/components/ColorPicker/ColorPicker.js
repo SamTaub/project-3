b5d2faf289ded82cc479fd373fad282116c36dee
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-14 08:31:08 -0400

    Refactor ColorPicker so that colors appear 5 to a row

--- client/src/components/ColorPicker/ColorPicker.js
+++ client/src/components/ColorPicker/ColorPicker.js
@@ -4,37 +4,38 @@ import "./style.css";
 
 function ColorPicker(props) {
   let colorArray = Object.entries(colorPalette);
-  let table = [];
 
-  for (let i = 0; i < colorArray.length; i++) {
-    console.log(colorArray[i]);
-    if ((i + 1) % 5 === 0) {
-      table.push(
-        <tr>
-          <td
-            data-value={colorArray[i][0]}
-            style={{ background: `${colorArray[i][0]}`, width: "5px" }}
-          >
-            {colorArray[i][1]}
-          </td>
-        </tr>
-      );
-    } else {
-      table.push(
+  const groupSize = 5;
+
+  const groups = colorArray
+    .map((item, index) => {
+      return index % groupSize === 0
+        ? colorArray.slice(index, index + groupSize)
+        : null;
+    })
+    .filter(item => item);
+
+  let body = groups.map(row => {
+    const colors = row.map(item => {
+      console.log(item);
+      return (
         <td
-          data-value={colorArray[i][0]}
-          style={{ background: `${colorArray[i][0]}` }}
+          key={item[0]}
+          data-value={item[0]}
+          style={{ background: `${item[0]}`, width: "5px" }}
         >
-          {colorArray[i][1]}
+          {item[1]}
         </td>
       );
-    }
-  }
+    });
+
+    return <tr>{colors}</tr>;
+  });
 
   return (
     <div className="table-responsive" onClick={props.onChange}>
       <table className="table">
-        <tbody>{table}</tbody>
+        <tbody>{body}</tbody>
       </table>
     </div>
   );
