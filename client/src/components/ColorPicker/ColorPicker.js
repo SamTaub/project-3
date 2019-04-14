f7787ec8a3e143c6cec168f747eb8cec8da5de4b
sdreyes <sheshechan90@gmail.com>
2019-04-14 16:41:02 -0400

    Adjust styling of the color picker to render RGBA value instead of hex code colors

--- client/src/components/ColorPicker/ColorPicker.js
+++ client/src/components/ColorPicker/ColorPicker.js
@@ -22,7 +22,7 @@ function ColorPicker(props) {
         <td
           key={item[0]}
           data-value={item[0]}
-          style={{ background: `${item[0]}`, padding: "4px" }}
+          style={{ background: `rgba(${item[0]})`, padding: "4px" }}
         >
           {item[1]}
         </td>
