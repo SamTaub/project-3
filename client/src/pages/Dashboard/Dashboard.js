a641e8488bc0ccd578fd400ee596fff2629773d0
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2019-04-14 16:09:14 -0400

    Initialize dashboard page

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import { Container, Row, Col } from "../../components/Grid";
 
-function NoMatch() {
+function Dashboard() {
     return (
-        <div>
-            This is the protected dashboard.
-        </div>
+        <Container styles="well p-3">
+            <Row>
+                <Col size="12">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Published</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Drafts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Favorites</a>
+                        </li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
-export default NoMatch;
+export default Dashboard;
