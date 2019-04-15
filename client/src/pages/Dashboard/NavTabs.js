96dea696ef8548876f844b96fb3065f6e4e03e92
sdreyes <sheshechan90@gmail.com>
2019-04-15 11:56:58 -0400

    Create tab component design for dashboard page

---
+++ client/src/pages/Dashboard/NavTabs.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function NavTabs(props) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#published"
+          onClick={() => props.handleTabClick("Published")}
+          className={props.currentTab === "Published" ? "nav-link active" : "nav-link"}
+        >
+          Published
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#drafts"
+          onClick={() => props.handleTabClick("Drafts")}
+          className={props.currentTab === "Drafts" ? "nav-link active" : "nav-link"}
+        >
+          Drafts
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#favorites"
+          onClick={() => props.handleTabClick("Favorites")}
+          className={props.currentTab === "Favorites" ? "nav-link active" : "nav-link"}
+        >
+          Favorites
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
