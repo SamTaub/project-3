fa49138858bea81f0176e4796adcc7a54d31afba
sdreyes <sheshechan90@gmail.com>
2019-04-21 13:13:03 -0400

    Change view button to an <a> tag to allow linking to the detail page and render buttons for the browse page and favorites page

--- client/src/components/DesignCard/index.js
+++ client/src/components/DesignCard/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { ButtonGroup } from "../Board/Board";
-import {TrashButton, EditButton, PublishButton, ViewButton, UnpublishButton} from "../DashboardButtons/DashboardButtons";
+import {TrashButton, EditButton, PublishButton, ViewButton, UnpublishButton, FavoriteButton, UnfavoriteButton } from "../DashboardButtons/DashboardButtons";
 console.log("hello design card");
 
 class DesignCard extends Component {
@@ -26,7 +26,7 @@ class DesignCard extends Component {
                 return(
                     <ButtonGroup
                     // NOTE: Button1 will need to be changed to a link to the design detail page
-                        button1={<Link to={`/design/${this.props.id}`}><ViewButton onClick={event => this.props.view(event, this.props.id)} id={this.props.id}/></Link>} 
+                        button1={<ViewButton id={this.props.id} link={`/design/${this.props.id}`}/>}
                         button2={<UnpublishButton onClick={event => this.props.unpublish(event, this.props.id)} id={this.props.id}/>}
                         button3={<TrashButton onClick={event => this.props.delete(event, this.props.id)} id={this.props.id}/>}
                     />
@@ -40,16 +40,29 @@ class DesignCard extends Component {
                     />
                 )
             case "favorites":
-                break;
+                return (
+                    <ButtonGroup
+                        button1={<ViewButton id={this.props.id} link={`/design/${this.props.id}`}/>}
+                        button2={<UnfavoriteButton onClick={event => this.props.unfavorite(event, this.props.id, this.props.userId)} id={this.props.id}/>}
+                        button3={<EditButton onClick={event => this.props.edit(event, this.props.id)} id={this.props.id}/>}
+                    />
+                )
             case "browse":
-                break;
+                return (
+                    <ButtonGroup
+                        button1={<ViewButton id={this.props.id} link={`/design/${this.props.id}`}/>}
+                        // To do: Button 2 needs to be dynamically changed based on whether or not that design is already saved to the user's favorites.
+                        button2={<FavoriteButton onClick={event => this.props.favorite(event, this.props.id, this.props.userId)} id={this.props.id}/>}
+                        button3={<EditButton onClick={event => this.props.edit(event, this.props.id)} id={this.props.id}/>}
+                    />
+                )
         }
     }
 
     render() {
         return (
             <div className="card mb-3">
-                <img src={this.props.img} style={{ imageRendering: "pixelated" }} className="card-img-top p-3" alt={this.props.title} />
+                <img src={this.props.img} className="card-img-top p-3 designPreview" alt={this.props.title} />
                 <div className="card-body">
                     <h6 className={"card-title font-weight-bold text-truncate"}>{this.props.title}</h6>
                     <p className="card-text text-truncate">{this.props.description}</p>
