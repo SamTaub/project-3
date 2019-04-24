61027dc614cd95e06b17737522e5e4b0fabbfb69
sdreyes <sheshechan90@gmail.com>
2019-04-24 12:03:43 -0400

    Adjust indentation

--- client/src/pages/Browse/Browse.js
+++ client/src/pages/Browse/Browse.js
@@ -12,193 +12,193 @@ import { Form } from "react-bootstrap";
 import "./style.css";
 
 class Browse extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            publishedDesigns: [],
-            sort: "Newest",
-            category: "All",
-            difficulty: "All",
-            // rating: "",
-            currentUser: "",
-            usersFavorites: []
-        }
+    this.state = {
+      publishedDesigns: [],
+      sort: "Newest",
+      category: "All",
+      difficulty: "All",
+      // rating: "",
+      currentUser: "",
+      usersFavorites: []
     }
+  }
 
-    componentDidMount() {
-        userAPI
-            .checkAuthStatus()
-            .then(res => {
-                console.log(res.data.id);
-                this.setState({ currentUser: res.data.id }, () => this.checkUserFavorites());
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
+  componentDidMount() {
+    userAPI
+      .checkAuthStatus()
+      .then(res => {
+        console.log(res.data.id);
+        this.setState({ currentUser: res.data.id }, () => this.checkUserFavorites());
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    checkUserFavorites() {
-        if (this.state.currentUser) {
-            userAPI.findUserWithoutPopulation(this.state.currentUser)
-                .then(res => {
-                    this.setState({ usersFavorites: res.data.favorites }, () => this.getAllPublishedDesigns())
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }
-        else {
-            this.getAllPublishedDesigns();
-        }
-    }
-
-    getAllPublishedDesigns = () => {
-        designAPI
-            .getAllPublishedDesigns(
-                this.state.category,
-                this.state.difficulty,
-                this.state.sort
-            )
-            .then(res => {
-                this.setState({
-                    publishedDesigns: res.data
-                });
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
-
-    // We will also need an unfavorite event. The cards will eventually dynamically pass in either the favorite or unfavorite event based on whether or not the design is already in the user's favorites.
-    favoriteEvent = (event, userId, designId) => {
-        event.preventDefault();
-        if (!this.state.currentUser || this.state.currentUser === "") {
-            alert("You must be logged in to add a favorite!");
-        }
-        else {
-            dashboardAPI.addFavorite(userId, designId)
-                .then(res => {
-                    this.checkUserFavorites();
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }
-    }
-
-    unfavoriteEvent = (event, userId, designId) => {
-        event.preventDefault();
-        if (!this.state.currentUser || this.state.currentUser === "") {
-            alert("You must be logged in to add a favorite!");
-        }
-        else {
-            dashboardAPI.removeFavorite(userId, designId)
-                .then(res => {
-                    this.checkUserFavorites();
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }
-    }
-
-    editEvent = (event, id) => {
-        event.preventDefault();
-        alert("Edit feature coming soon!");
-    }
-
-    // Change handlers for Browse forms
-
-    handleSortChange = (event) => {
-        event.preventDefault();
-        this.setState({
-            sort: event.target.value
-        }, () => {
-            this.checkUserFavorites();
-            // console.log(`Sorting results by ${this.state.sort}`);
+  checkUserFavorites() {
+    if (this.state.currentUser) {
+      userAPI.findUserWithoutPopulation(this.state.currentUser)
+        .then(res => {
+          this.setState({ usersFavorites: res.data.favorites }, () => this.getAllPublishedDesigns())
+        })
+        .catch(err => {
+          console.log(err);
         })
     }
+    else {
+      this.getAllPublishedDesigns();
+    }
+  }
 
-    handleCategoryChange = (event) => {
-        event.preventDefault();
+  getAllPublishedDesigns = () => {
+    designAPI
+      .getAllPublishedDesigns(
+        this.state.category,
+        this.state.difficulty,
+        this.state.sort
+      )
+      .then(res => {
         this.setState({
-            category: event.target.value
-        }, () => {
-            this.checkUserFavorites();
-            // console.log(`Filtering results by ${this.state.category}`);
+          publishedDesigns: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  // We will also need an unfavorite event. The cards will eventually dynamically pass in either the favorite or unfavorite event based on whether or not the design is already in the user's favorites.
+  favoriteEvent = (event, userId, designId) => {
+    event.preventDefault();
+    if (!this.state.currentUser || this.state.currentUser === "") {
+      alert("You must be logged in to add a favorite!");
+    }
+    else {
+      dashboardAPI.addFavorite(userId, designId)
+        .then(res => {
+          this.checkUserFavorites();
+        })
+        .catch(err => {
+          console.log(err);
         })
     }
+  }
 
-    handleDifficultyChange = (event) => {
-        event.preventDefault();
-        this.setState({
-            difficulty: event.target.value
-        }, () => {
-            // Verify state change with console.log()
-            this.checkUserFavorites();
-            // console.log(`Filtering results by ${this.state.difficulty}`);
+  unfavoriteEvent = (event, userId, designId) => {
+    event.preventDefault();
+    if (!this.state.currentUser || this.state.currentUser === "") {
+      alert("You must be logged in to add a favorite!");
+    }
+    else {
+      dashboardAPI.removeFavorite(userId, designId)
+        .then(res => {
+          this.checkUserFavorites();
+        })
+        .catch(err => {
+          console.log(err);
         })
     }
+  }
 
-    // handleRatingChange = (event) => {
-    //     event.preventDefault();
-    //     this.setState({
-    //         rating: event.target.value
-    //     }, () =>{
-    //         // Verify state change with console.log()
-    //         console.log(`Filtering results by ${this.state.rating}`);
-    //     })
-    // }
+  editEvent = (event, id) => {
+    event.preventDefault();
+    alert("Edit feature coming soon!");
+  }
 
-    render() {
-        return (
-            <Container styles="well p-3">
-                {/* <Row styles="p-3 justify-content-center">
+  // Change handlers for Browse forms
+
+  handleSortChange = (event) => {
+    event.preventDefault();
+    this.setState({
+      sort: event.target.value
+    }, () => {
+      this.checkUserFavorites();
+      // console.log(`Sorting results by ${this.state.sort}`);
+    })
+  }
+
+  handleCategoryChange = (event) => {
+    event.preventDefault();
+    this.setState({
+      category: event.target.value
+    }, () => {
+      this.checkUserFavorites();
+      // console.log(`Filtering results by ${this.state.category}`);
+    })
+  }
+
+  handleDifficultyChange = (event) => {
+    event.preventDefault();
+    this.setState({
+      difficulty: event.target.value
+    }, () => {
+      // Verify state change with console.log()
+      this.checkUserFavorites();
+      // console.log(`Filtering results by ${this.state.difficulty}`);
+    })
+  }
+
+  // handleRatingChange = (event) => {
+  //     event.preventDefault();
+  //     this.setState({
+  //         rating: event.target.value
+  //     }, () =>{
+  //         // Verify state change with console.log()
+  //         console.log(`Filtering results by ${this.state.rating}`);
+  //     })
+  // }
+
+  render() {
+    return (
+      <Container styles="well p-3">
+        {/* <Row styles="p-3 justify-content-center">
                     <h1>Browse</h1>
                 </Row> */}
-                <div className="row pt-2 pr-5 pl-5 mb-3 sticky-top bg-light rounded">
-                    <Col size="4">
-                        <SortBy onChange={this.handleSortChange}></SortBy>
-                    </Col>
-                    <Col size="4">
-                        <CategoryForm onChange={this.handleCategoryChange}></CategoryForm>
-                    </Col>
-                    <Col size ="4">
-                        <DifficultyForm onChange={this.handleDifficultyChange}></DifficultyForm>
-                    </Col>
-                            {/* <RatingForm onChange={this.handleRatingChange}></RatingForm> */}
-                </div>
-                <div className="row">
-                    {!this.state.publishedDesigns.length > 0
-                        ? (
-                            <Col size="12">No published designs to display</Col>
-                        ) : (
-                            this.state.publishedDesigns.map(design => {
-                                console.log(this.state.publishedDesigns);
-                                return (
-                                    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12" key={design._id + 1}>
-                                        <DesignCard
-                                            key={design._id}
-                                            id={design._id}
-                                            currentUser={this.state.currentUser}
-                                            img={design.canvasImage}
-                                            title={design.title}
-                                            description={design.description}
-                                            favorite={this.favoriteEvent}
-                                            unfavorite={this.unfavoriteEvent}
-                                            edit={this.editEvent}
-                                            page={"browse"}
-                                            isFavorite={this.state.usersFavorites.indexOf(design._id) > -1 ? true : false}
-                                        />
-                                    </div>
-                                );
-                            })
-                        )}
-                </div>
-            </Container>
-        )
-    }
+        <div className="row pt-2 pr-5 pl-5 mb-3 sticky-top bg-light rounded">
+          <Col size="4">
+            <SortBy onChange={this.handleSortChange}></SortBy>
+          </Col>
+          <Col size="4">
+            <CategoryForm onChange={this.handleCategoryChange}></CategoryForm>
+          </Col>
+          <Col size="4">
+            <DifficultyForm onChange={this.handleDifficultyChange}></DifficultyForm>
+          </Col>
+          {/* <RatingForm onChange={this.handleRatingChange}></RatingForm> */}
+        </div>
+        <div className="row">
+          {!this.state.publishedDesigns.length > 0
+            ? (
+              <Col size="12">No published designs to display</Col>
+            ) : (
+              this.state.publishedDesigns.map(design => {
+                console.log(this.state.publishedDesigns);
+                return (
+                  <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12" key={design._id + 1}>
+                    <DesignCard
+                      key={design._id}
+                      id={design._id}
+                      currentUser={this.state.currentUser}
+                      img={design.canvasImage}
+                      title={design.title}
+                      description={design.description}
+                      favorite={this.favoriteEvent}
+                      unfavorite={this.unfavoriteEvent}
+                      edit={this.editEvent}
+                      page={"browse"}
+                      isFavorite={this.state.usersFavorites.indexOf(design._id) > -1 ? true : false}
+                    />
+                  </div>
+                );
+              })
+            )}
+        </div>
+      </Container>
+    )
+  }
 
 
 };
