3bb65bfc2565c3ecdc7325eec0453fb0fb5b37f1
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-24 19:40:01 -0400

    Conditionally render homepage button based on auth status

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,14 +3,33 @@ import { Link } from "react-router-dom";
 import { Container, Row, Col } from "../../components/Grid";
 import "./style.css";
 
-function Home() {
+function Home(props) {
   return (
     <Container className="bg-transparent">
       <Row styles="justify-content-center align-items-center text-center text-white">
         <Col size="lg-8">
-          <h1 className="mt-5" id="mast">Beadli</h1>
-            <p id="subtitle">Make interesting designs with beads.</p>
-            <Link className="btn btn-outline-light btn-lg" to="/login" role="button">Get Started</Link>
+          <h1 className="mt-5" id="mast">
+            Beadli
+          </h1>
+          <p id="subtitle">Make interesting designs with beads.</p>
+          {props.isAuthed ? (
+            <Link
+              className="btn btn-outline-light btn-lg"
+              to="/create"
+              role="button"
+            >
+              Create a Design
+            </Link>
+          ) : (
+            <Link
+              className="btn btn-outline-light btn-lg"
+              to="/login"
+              role="button"
+            >
+              Get Started
+            </Link>
+          )}
+          {/* <Link className="btn btn-outline-light btn-lg" to="/login" role="button">Get Started</Link> */}
         </Col>
       </Row>
     </Container>
