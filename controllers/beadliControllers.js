77406648dfa107333d19c2f348b7fea9d87c8ddb
sdreyes <sheshechan90@gmail.com>
2019-04-25 11:18:45 -0400

    Modify 'find designs by user' function to only retrieve published designs

--- controllers/beadliControllers.js
+++ controllers/beadliControllers.js
@@ -1,229 +1,220 @@
-const db = require ('../models');
+const db = require('../models');
 
 module.exports = {
 
-    // DESIGNS
+  // DESIGNS
 
-    // Find all designs
-    findAll: function(req, res) {
-        console.log("Getting all designs");
-        db.Design
-            .find({})
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
+  // Find all designs
+  findAll: function (req, res) {
+    db.Design
+      .find({})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
 
-    // Find all published designs
-    findAllPublished: function(req, res) {
-        let query = {};
-        let sortVal = -1;
-        query["$and"] = [{ "published": true }];
+  // Find all published designs
+  findAllPublished: function (req, res) {
+    let query = {};
+    let sortVal = -1;
+    query["$and"] = [{ "published": true }];
 
-        if (req.params.category !== "All") {
-            query['$and'].push({ "category": req.params.category });
-        }
-
-        if (req.params.difficulty !== "All") {
-            query['$and'].push({ "difficulty": req.params.difficulty });
-        }
-
-        if (req.params.sort === "Oldest") {
-            sortVal = 1
-        }
-
-        // console.log(query);
-
-        db.Design.find(query)
-          .sort({ _id: sortVal })
-          .then(dbModel => res.json(dbModel))
-          .catch(err => res.json(err));
-    },
-
-    // Find a design based on it's ID
-    findById: function(req, res) {
-        console.log("Getting design");
-        db.Design
-            .findById(req.params.id)
-            .populate("comments")
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    findByUser: function(req, res) {
-      console.log("Finding designs by user");
-      db.Design
-        .find({ "userId": req.params.id })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.json(err));
-    },
-
-    //  Find a design by a title 'like' the query
-    //  Note:  Not sure if this will work cause req is not being used but this is what the docs say
-    findByTitle: function(req, res) {
-        db.Design
-            .find({ "title": new RegExp("^" + req.params.title + "$", "i") })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Find a design based on its category
-    findByCategory: function(req, res) {
-        db.Design
-            .find({ "category": req.query })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Find a design based on its rating
-    findByRating: function(req, res){
-        db.Design
-            .find({ "rating": req.query })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Create a design
-    createDesign: function(req, res) {
-        db.Design
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Update a design
-    updateDesign: function(req, res) {
-        db.Design
-            .findOneAndUpdate({ _id: req.params.id }, req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Remove a design
-    removeDesign: function(req, res) {
-        db.Design
-            .findById({ _id: req.params.id })
-            .then(dbModel => dbModel.remove())
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    //Publish a design
-    publishDesign: function(req, res) {
-        console.log(req.body);
-        db.Design.findOneAndUpdate(
-          { _id: req.params.id },
-          {
-            $set: {
-              published: true,
-              title: req.body.title,
-              description: req.body.description,
-              difficulty: req.body.difficulty,
-              category: req.body.category
-            }
-          }
-        )
-          .then(dbModel => res.json(dbModel))
-          .catch(err => res.json(err));
-    },
-
-    // Unpublish a design
-    unpublishDesign: function(req, res) {
-        db.Design
-            .findOneAndUpdate({"_id": req.params.id }, { 
-                $set: { "published": false}
-            })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // COMMENTS
-
-    // Create a comment
-    createComment: function(req, res) {
-        db.Comment
-            .create(req.body)
-            .then(dbModel => { db.Design.findOneAndUpdate({ "_id": req.params.id }, { $push: { "comments": dbModel._id } }, { new: true }) })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Update/edit a comment
-    updateComment: function(req, res) {
-        db.Comment
-            .findByIdAndUpdate({ "_id": req.params.id }, req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Delete a comment
-    deleteComment: function(req, res){
-        db.Comment
-            .deleteOne({ "_id": req.params.commentId })
-            .then(() => {
-                return db.Design.update({ "_id": req.params.designId }, { $pull: { comments: req.params.commentId } })
-            })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Controllers for Profile page
-
-    findDraft: function(req, res){
-        db.Design
-            .find({ "userId": req.params.id, "published": false })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    findPublished: function(req, res){
-        console.log("getting published designs test");
-        db.Design
-            .find({ "userId": req.params.id, "published": true })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    //  Add favorites to an associated user
-    
-    findFavorites: function(req, res){
-        db.User
-        .find({ "_id": req.params.id })
-        .populate("favorites")
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.json(err));
-    },
-    
-    addFavorite: function(req, res){
-        console.log(`adding ${req.params.designId} to ${req.params.userId}'s favorites`);
-        db.User
-            .findOneAndUpdate({ "_id": req.params.userId }, { $push: { "favorites": req.params.designId } }, { new: true })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    removeFavorite: function(req, res){
-        console.log(`removing ${req.params.designId} from ${req.params.userId}'s favorites`);
-        db.User
-            .findOneAndUpdate({ "_id": req.params.userId }, { $pull: { "favorites": req.params.designId } })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.json(err));
-    },
-
-    // Find one user to display their dashboard
-
-    findUser: function(req, res){
-        db.User
-        .findOne({"_id": req.params.id})
-        .populate("favorites")
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.json(err));
-    },
-
-    findUserWithoutPopulation: function(req, res){
-        db.User
-        .findOne({"_id": req.params.id})
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.json(err));
+    if (req.params.category !== "All") {
+      query['$and'].push({ "category": req.params.category });
     }
+
+    if (req.params.difficulty !== "All") {
+      query['$and'].push({ "difficulty": req.params.difficulty });
+    }
+
+    if (req.params.sort === "Oldest") {
+      sortVal = 1
+    }
+
+    db.Design.find(query)
+      .sort({ _id: sortVal })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Find a design based on it's ID
+  findById: function (req, res) {
+    db.Design
+      .findById(req.params.id)
+      .populate("comments")
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  findByUser: function (req, res) {
+    db.Design
+      .find({ "userId": req.params.id, "published": true })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  //  Find a design by a title 'like' the query
+  //  Note:  Not sure if this will work cause req is not being used but this is what the docs say
+  findByTitle: function (req, res) {
+    db.Design
+      .find({ "title": new RegExp("^" + req.params.title + "$", "i") })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Find a design based on its category
+  findByCategory: function (req, res) {
+    db.Design
+      .find({ "category": req.query })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Find a design based on its rating
+  findByRating: function (req, res) {
+    db.Design
+      .find({ "rating": req.query })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Create a design
+  createDesign: function (req, res) {
+    db.Design
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Update a design
+  updateDesign: function (req, res) {
+    db.Design
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Remove a design
+  removeDesign: function (req, res) {
+    db.Design
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  //Publish a design
+  publishDesign: function (req, res) {
+    db.Design.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          published: true,
+          title: req.body.title,
+          description: req.body.description,
+          difficulty: req.body.difficulty,
+          category: req.body.category
+        }
+      }
+    )
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Unpublish a design
+  unpublishDesign: function (req, res) {
+    db.Design
+      .findOneAndUpdate({ "_id": req.params.id }, {
+        $set: { "published": false }
+      })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // COMMENTS
+
+  // Create a comment
+  createComment: function (req, res) {
+    db.Comment
+      .create(req.body)
+      .then(dbModel => { db.Design.findOneAndUpdate({ "_id": req.params.id }, { $push: { "comments": dbModel._id } }, { new: true }) })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Update/edit a comment
+  updateComment: function (req, res) {
+    db.Comment
+      .findByIdAndUpdate({ "_id": req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Delete a comment
+  deleteComment: function (req, res) {
+    db.Comment
+      .deleteOne({ "_id": req.params.commentId })
+      .then(() => {
+        return db.Design.update({ "_id": req.params.designId }, { $pull: { comments: req.params.commentId } })
+      })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Controllers for Profile page
+
+  findDraft: function (req, res) {
+    db.Design
+      .find({ "userId": req.params.id, "published": false })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  findPublished: function (req, res) {
+    db.Design
+      .find({ "userId": req.params.id, "published": true })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  //  Add favorites to an associated user
+
+  findFavorites: function (req, res) {
+    db.User
+      .find({ "_id": req.params.id })
+      .populate("favorites")
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  addFavorite: function (req, res) {
+    db.User
+      .findOneAndUpdate({ "_id": req.params.userId }, { $push: { "favorites": req.params.designId } }, { new: true })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  removeFavorite: function (req, res) {
+    db.User
+      .findOneAndUpdate({ "_id": req.params.userId }, { $pull: { "favorites": req.params.designId } })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  // Find one user to display their dashboard
+
+  findUser: function (req, res) {
+    db.User
+      .findOne({ "_id": req.params.id })
+      .populate("favorites")
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
+
+  findUserWithoutPopulation: function (req, res) {
+    db.User
+      .findOne({ "_id": req.params.id })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  }
 
 };
