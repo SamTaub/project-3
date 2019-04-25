2522b80903111bc4a413eea9ce5e36af9049fe0f
sdreyes <sheshechan90@gmail.com>
2019-04-25 13:27:05 -0400

    Add styling to the 404 page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {Container, Row } from "../components/Grid";
 
 function NoMatch() {
     return (
-        <div>
-            404 Not Found
-        </div>
+        <Container className="bg-transparent p-5">
+          <Row styles="justify-content-center align-items-center text-center text-white">
+            <div className="col-12 text-center">
+              <h1 style={{fontFamily: "Beadli", fontSize: "10em"}}>404</h1>
+              <h1>Page Not Found</h1>
+              <br/>
+              <Link to="/" className="btn btn-lg btn-outline-light">Back to Home</Link>
+            </div>
+          </Row>
+        </Container>
     );
 };
 
