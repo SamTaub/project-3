de9704964553cb7f8d24b1b6f3e7eefab1c2d4f0
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-05-20 20:25:11 -0400

    Improve code indentation

--- client/src/pages/Browse/Browse.js
+++ client/src/pages/Browse/Browse.js
@@ -14,7 +14,7 @@ import "./style.css";
 
 class Browse extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       publishedDesigns: [],
@@ -27,7 +27,7 @@ class Browse extends Component {
       scrolling: false,
       modalShow: false,
       modalNotification: ""
-    }
+    };
   }
 
   componentDidMount() {
@@ -35,7 +35,9 @@ class Browse extends Component {
     userAPI
       .checkAuthStatus()
       .then(res => {
-        this.setState({ currentUser: res.data.id }, () => this.checkUserFavorites());
+        this.setState({ currentUser: res.data.id }, () =>
+          this.checkUserFavorites()
+        );
       })
       .catch(err => {
         console.log(err);
@@ -44,19 +46,21 @@ class Browse extends Component {
 
   componentWillUnmount = () => {
     window.removeEventListener("scroll", this.handleScroll);
-  }
+  };
 
   checkUserFavorites() {
     if (this.state.currentUser) {
-      userAPI.findUserWithoutPopulation(this.state.currentUser)
+      userAPI
+        .findUserWithoutPopulation(this.state.currentUser)
         .then(res => {
-          this.setState({ usersFavorites: res.data.favorites }, () => this.getAllPublishedDesigns())
+          this.setState({ usersFavorites: res.data.favorites }, () =>
+            this.getAllPublishedDesigns()
+          );
         })
         .catch(err => {
           console.log(err);
-        })
-    }
-    else {
+        });
+    } else {
       this.getAllPublishedDesigns();
     }
   }
@@ -76,89 +80,97 @@ class Browse extends Component {
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
   favoriteEvent = (event, userId, designId) => {
     event.preventDefault();
     if (!this.state.currentUser || this.state.currentUser === "") {
-      this.setState({ 
-        modalShow: true, 
-        modalNotification: "You must be logged in to add a favorite!" 
+      this.setState({
+        modalShow: true,
+        modalNotification: "You must be logged in to add a favorite!"
       });
-    }
-    else {
-      dashboardAPI.addFavorite(userId, designId)
+    } else {
+      dashboardAPI
+        .addFavorite(userId, designId)
         .then(res => {
           this.checkUserFavorites();
         })
         .catch(err => {
           console.log(err);
-        })
+        });
     }
-  }
+  };
 
   unfavoriteEvent = (event, userId, designId) => {
     event.preventDefault();
     if (!this.state.currentUser || this.state.currentUser === "") {
-      this.setState({ 
-        modalShow: true, 
-        modalNotification: "You must be logged in to add a favorite!" 
+      this.setState({
+        modalShow: true,
+        modalNotification: "You must be logged in to add a favorite!"
       });
-    }
-    else {
-      dashboardAPI.removeFavorite(userId, designId)
+    } else {
+      dashboardAPI
+        .removeFavorite(userId, designId)
         .then(res => {
           this.checkUserFavorites();
         })
         .catch(err => {
           console.log(err);
-        })
+        });
     }
-  }
+  };
 
   modalClose = () => this.setState({ modalShow: false });
 
   editEvent = (event, id) => {
     event.preventDefault();
     alert("Edit feature coming soon!");
-  }
+  };
 
   // Change handlers for Browse forms
-  handleSortChange = (event) => {
+  handleSortChange = event => {
     event.preventDefault();
-    this.setState({
-      sort: event.target.value
-    }, () => {
-      this.checkUserFavorites();
-    })
-  }
+    this.setState(
+      {
+        sort: event.target.value
+      },
+      () => {
+        this.checkUserFavorites();
+      }
+    );
+  };
 
-  handleCategoryChange = (event) => {
+  handleCategoryChange = event => {
     event.preventDefault();
-    this.setState({
-      category: event.target.value
-    }, () => {
-      this.checkUserFavorites();
-    })
-  }
+    this.setState(
+      {
+        category: event.target.value
+      },
+      () => {
+        this.checkUserFavorites();
+      }
+    );
+  };
 
-  handleDifficultyChange = (event) => {
+  handleDifficultyChange = event => {
     event.preventDefault();
-    this.setState({
-      difficulty: event.target.value
-    }, () => {
-      this.checkUserFavorites();
-    })
-  }
+    this.setState(
+      {
+        difficulty: event.target.value
+      },
+      () => {
+        this.checkUserFavorites();
+      }
+    );
+  };
 
   handleScroll = () => {
     if (window.scrollY <= 250 && this.state.scrolling === true) {
-      this.setState({scrolling: false});
+      this.setState({ scrolling: false });
+    } else if (window.scrollY > 250 && this.state.scrolling !== true) {
+      this.setState({ scrolling: true });
     }
-    else if (window.scrollY > 250 && this.state.scrolling !== true) {
-      this.setState({scrolling: true});
-    }
-  }
+  };
 
   // handleRatingChange = (event) => {
   //     event.preventDefault();
@@ -179,47 +191,59 @@ class Browse extends Component {
               <h1 className="text-center">Browse</h1>
             </Col>
           </Row>
-          
-          <div className="row pr-5 pl-5 pt-2 mb-3 sticky-top rounded" onScroll={this.handleScroll} style={{background: this.state.scrolling ? "#f8f9fa" : "transparent"}}>
+
+          <div
+            className="row pr-5 pl-5 pt-2 mb-3 sticky-top rounded"
+            onScroll={this.handleScroll}
+            style={{
+              background: this.state.scrolling ? "#f8f9fa" : "transparent"
+            }}
+          >
             <Col size="4">
-              <SortBy onChange={this.handleSortChange}></SortBy>
+              <SortBy onChange={this.handleSortChange} />
             </Col>
             <Col size="4">
-              <CategoryForm onChange={this.handleCategoryChange}></CategoryForm>
+              <CategoryForm onChange={this.handleCategoryChange} />
             </Col>
             <Col size="4">
-              <DifficultyForm onChange={this.handleDifficultyChange}></DifficultyForm>
+              <DifficultyForm onChange={this.handleDifficultyChange} />
             </Col>
             {/* <RatingForm onChange={this.handleRatingChange}></RatingForm> */}
           </div>
           <div className="row">
-            {!this.state.publishedDesigns.length > 0
-              ? (
-                <Col size="12">No published designs to display</Col>
-              ) : (
-                this.state.publishedDesigns.map(design => {
-                  return (
-                    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12" key={design._id + 1}>
-                      <DesignCard
-                        key={design._id}
-                        id={design._id}
-                        currentUser={this.state.currentUser}
-                        img={design.canvasImage}
-                        title={design.title}
-                        description={design.description}
-                        favorite={this.favoriteEvent}
-                        unfavorite={this.unfavoriteEvent}
-                        edit={this.editEvent}
-                        page={"browse"}
-                        isFavorite={this.state.usersFavorites.indexOf(design._id) > -1 ? true : false}
-                      />
-                    </div>
-                  );
-                })
-              )}
+            {!this.state.publishedDesigns.length > 0 ? (
+              <Col size="12">No published designs to display</Col>
+            ) : (
+              this.state.publishedDesigns.map(design => {
+                return (
+                  <div
+                    className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12"
+                    key={design._id + 1}
+                  >
+                    <DesignCard
+                      key={design._id}
+                      id={design._id}
+                      currentUser={this.state.currentUser}
+                      img={design.canvasImage}
+                      title={design.title}
+                      description={design.description}
+                      favorite={this.favoriteEvent}
+                      unfavorite={this.unfavoriteEvent}
+                      edit={this.editEvent}
+                      page={"browse"}
+                      isFavorite={
+                        this.state.usersFavorites.indexOf(design._id) > -1
+                          ? true
+                          : false
+                      }
+                    />
+                  </div>
+                );
+              })
+            )}
           </div>
         </Container>
-        <SimpleModal 
+        <SimpleModal
           show={this.state.modalShow}
           onHide={this.modalClose}
           title="Log in required"
@@ -229,8 +253,8 @@ class Browse extends Component {
           buttonActionFunc={this.modalClose}
         />
       </Fragment>
-    )
+    );
   }
-};
+}
 
 export default Browse;
