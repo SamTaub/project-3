a13d2fb8fce65e219bcee3ff4e79c705fd101310
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-05-20 20:35:29 -0400

    Add number of favorites to the Design model

--- models/Design.js
+++ models/Design.js
@@ -1,45 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const designSchema = new Schema({
-    grid: {
-        type: Array
-    },
-    userId: {
-        type: String
-    },
-    title: {
-        type: String,
-        default: "Untitled"
-    },
-    category: {
-        type: String,
-        default: "Miscellaneous"
-    },
-    difficulty: {
-        type: String,
-        default: "Normal"
-    },
-    rating: {
-        type: Number,
-        default: 0
-    },
-    description: {
-        type: String,
-        default: "No description"
-    },
-    published: {
-        type: Boolean,
-        default: false
-    },
-    canvasImage: {
-        type: String,
-        default: "https://via.placeholder.com/400?text=No+Preview"
-    },
-    comments: [{
-        type: Schema.Types.ObjectId,
-        ref: "Comment"
-    }]
+  grid: {
+    type: Array
+  },
+  userId: {
+    type: String
+  },
+  title: {
+    type: String,
+    default: "Untitled"
+  },
+  category: {
+    type: String,
+    default: "Miscellaneous"
+  },
+  difficulty: {
+    type: String,
+    default: "Normal"
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  description: {
+    type: String,
+    default: "No description"
+  },
+  published: {
+    type: Boolean,
+    default: false
+  },
+  numFavorites: {
+    type: Number,
+    default: 0
+  },
+  canvasImage: {
+    type: String,
+    default: "https://via.placeholder.com/400?text=No+Preview"
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
 
 const Design = mongoose.model("Design", designSchema);
